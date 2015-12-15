3a69a6d28de7653ddf74871f2ab0a9cfaec20352
evan <909798432@qq.com>
2015-12-15 23:50:52 +0800

    change static dir

--- graduation_project/settings.py
+++ graduation_project/settings.py
@@ -106,3 +106,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'sharephotos/static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'sharephotos/static'),
+    'sharephotos/static',
+)
