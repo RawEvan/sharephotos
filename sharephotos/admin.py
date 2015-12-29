66b6d897913becbc6bc9504b8263a17f08cdaadc
evan <909798432@qq.com>
2015-12-29 13:01:14 +0800

    change homepage, seperate some function from views.py

--- sharephotos/admin.py
+++ sharephotos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from sharephotos.models import tb_photo_info, tb_tag
+
+admin.site.register(tb_photo_info)
+admin.site.register(tb_tag)
