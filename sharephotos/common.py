0e1cd3e3483cb67840cae01af3cd0a3851fede0c
evan <909798432@qq.com>
2016-01-04 02:17:25 +0800

    change the size of thumbnails

--- sharephotos/common.py
+++ sharephotos/common.py
@@ -3,6 +3,6 @@
 
 def get_thumbnail_url(original_url):
         split_url = original_url.split('/')
-        size = 'w_300,h_180'
+        size = 'w_250,h_150'
         thumbnail_url = 'http://imgx.' + split_url[2] + '/' +split_url[3] + '/' + size + '/' + split_url[4]
         return thumbnail_url
