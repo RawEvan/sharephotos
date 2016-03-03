a516e73d66c4081f6fac62e8219ce1819cf224b0
evan <909798432@qq.com>
2016-03-03 18:32:05 +0800

    change urls:\d -> \d+

--- graduation_project/urls.py
+++ graduation_project/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     url(r'^photo/$', 'sharephotos.views.photo', name = 'photo'),
     url(r'^face/$', 'sharephotos.views.face', name = 'face'),
     url(r'^photo_manage', 'sharephotos.views.photoManage'),
-    url(r'^delete/(\d)', 'sharephotos.views.delete'),
+    url(r'^delete/(\d+)/$', 'sharephotos.views.delete'),
     url(r'^accounts/', include('users.urls')),
 ]
