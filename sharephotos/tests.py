b41fd5f4330e6a56507adcae27e7caaeccef4440
evan <909798432@qq.com>
2016-03-24 17:02:11 +0800

    add unittest for savPhotoAndTag

--- sharephotos/tests.py
+++ sharephotos/tests.py
@@ -1,9 +1,39 @@
 from django.test import TestCase
 from django.http import HttpResponse
 from django.core.mail import send_mail
+from models import tb_photo_info, tb_tag
+import dbControl
 import time
-# Create your tests here.
+
+'''
 def EmailTest(request):
     message = '%s, Email from sharephotos.sinaapp.com' % time.ctime()
     send_mail('Subject_test', message, 'sys_sharephotos@sina.com', ['909798432@qq.com'])
     return HttpResponse(u'send success')
+'''
+
+class dbTestCase(TestCase):
+    def setUp(self):
+        self.url = 'http://testStoreUrl.jpg'
+        self.description = 'testDescription'
+        self.tagList = ['tag1', 'tag2']
+        self.faceIdList = ['faceId1', 'faceId2']
+        self.allTagList = self.tagList + self.faceIdList
+        self.owner = 'testOwner'
+        dbControl.savePhotoAndTag(self.url, self.description, self.tagList, self.faceIdList, self.owner)
+    def testSavePhotoAndTag(self):
+        photoInfo = tb_photo_info.objects.get()
+        # get tag list
+        tagObjList= photoInfo.tb_tag_set.all()
+        gotTagList = []
+        for each_tag in tagObjList:
+            gotTagList.append(each_tag.tag)
+        gotInfoList = [photoInfo.store_url,
+                photoInfo.description,
+                gotTagList,
+                photoInfo.owner]
+        testInfoList = [self.url,
+                self.description,
+                self.allTagList,
+                self.owner]
+        self.assertEqual(gotInfoList, testInfoList)
